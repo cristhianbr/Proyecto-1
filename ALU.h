66cdc8e38a4a3c635c7548b8d8b1516617fc89d2
cristhianbr <cristhianbr1995@gmail.com>
2015-08-21 18:57:28 -0500

    Se crea la libreria de la unidad logica aritmetica con su respectivas funciones

---
+++ ALU.h
@@ -0,0 +1,11 @@
+void add(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma aritmetica
+
+void and(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la multiplicacion logica
+
+void eor(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma logica exclusiva
+
+void mov(unsigned long int *Rp, unsigned long int Rm);				            //funcion para mover un fichero a otra direccion
+
+void orr(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma logica
+
+void sub(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la resta aritmetica
