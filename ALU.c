8b38a6087b9201465f52b27c905da092ad82ca3d
lucho0718 <cristhianbr1995@gmail.com>
2015-08-25 10:49:17 -0500

    Se modifico el ALU.c para poder compilar y comprobar el funcionamineto adecuado de las funciones

--- ALU.c
+++ ALU.c
@@ -1,26 +1,26 @@
 #include "ALU.h"
 
-void add(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma aritmetica
+void add(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn)	//funcion de la suma aritmetica
 {
 	*Rp=Rm+Rn;
 }
-void and(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la multiplicacion logica
+void and(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn)	//funcion de la multiplicacion logica
 {
 	*Rp=Rm&Rn;
 }
-void eor(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma logica exclusiva
+void eor(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn)	//funcion de la suma logica exclusiva
 {
 	*Rp=Rm^Rm;
 }
-void mov(unsigned long int *Rp, unsigned long int Rm);				//funcion para mover un fichero a otra direccion
+void mov(unsigned long int *Rp, unsigned long int Rm)				//funcion para mover un fichero a otra direccion
 {
 	*Rp=Rm;
 }
-void orr(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma logica
+void orr(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn)	//funcion de la suma logica
 {
 	*Rp=Rm|Rn;
 }
-void sub(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la resta aritmetica
+void sub(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn)	//funcion de la resta aritmetica
 {
 	*Rp=Rm-Rn;
 }
