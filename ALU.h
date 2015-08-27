128932b2b8d7ce23a81dfbff803bca73f111df42
JuanDhz <jdhernandezs@uqvirtual.edu.co>
2015-08-27 16:07:16 -0500

    Se corrigio un error en la libreria donde una funcion salia como comentaro, se cambio el nombre de los parametros por unos mas signifimacitivos para el codigo.

--- ALU.h
+++ ALU.h
@@ -1,29 +1,30 @@
 /**
 *\brief Funcion que ejecuta una suma aritmetica.
 *\param *Rp Direccion en donde se guardara el resultado de la suma aritmetica.
-*\param Rm Registro a operar.
-*\param Rn Registro a operar.
+*\param Rm Registro 1 a operar.
+*\param Rn Registro 2 a operar.
+*/
 void add(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma aritmetica
 
 
 /**
 *\param *Rp Direccion en donde se guardara el resultado de la multiplicacion logica.
-*\param Rm Registro a operar.
-*\param Rn Registro a operar.
+*\param Rm Registro 1 a operar.
+*\param Rn Registro 2 a operar.
 */
 void and(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la multiplicacion logica
 
 /**
 *\param *Rp Direccion en donde se guardara el resultado de la suma logica exclusiva.
-*\param Rm Registro a operar.
-*\param Rn Registro a operar.
+*\param Rm Registro 1 a operar.
+*\param Rn Registro 2 a operar.
 */
 void eor(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la suma logica exclusiva
 
 /**
 *\param *Rp Direccion a la cual se desea mover el fichero.
-*\param Rm Registro a operar.
-*\param Rn Registro a operar.
+*\param Rm Registro 1 a operar.
+*\param Rn Registro 2 a operar.
 */
 void mov(unsigned long int *Rp, unsigned long int Rm);			//funcion para mover un fichero a otra direccion
 
@@ -35,8 +36,8 @@ void orr(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//f
 
 /**
 *\param *Rp Direccion en donde se guardara el resultado de la resta aritmetica.
-*\param Rm Registro a operar.
-*\param Rn Registro a operar.
+*\param Rm Registro 1 a operar.
+*\param Rn Registro 2 a operar.
 */
 void sub(unsigned long int *Rp, unsigned long int Rm, unsigned long int Rn);	//funcion de la resta aritmetica
 
