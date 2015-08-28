517919b75141de369cf1fd9e03f22b23d5dc0481
lucho0718 <lucho.leel@gmail.com>
2015-08-28 16:49:26 -0500

    se agregaron nuevos elementos a la funcion de desplazamientos

--- InstruccionesDesplazamiento.c
+++ InstruccionesDesplazamiento.c
@@ -1,8 +1,24 @@
+#include <math.h>
 #include "InstruccionesDesplazamiento.h"
-void ROR(uint32_t *Rdn, uint32_t Rm)
+void LSLS(uint32_t *Rnd, uint32_t Rm) //Desplazamiento logico a la izquierda.
 {
-	uint32_t R1, R2;
-	R1=*Rnd/(2^Rm);
-	R2=*Rnd*(2^(32-Rm));
-	*Rnd=R1+R2;
+	*Rnd=*Rnd*pow(2,Rm);
+}
+
+void LSLS(uint32_t *Rnd, uint32_t Rm) //Desplazamiento logico a la derecha.
+{
+	*Rnd=*Rnd/pow(2,Rm);
+}
+
+void ROR(uint32_t *Rdn, uint32_t Rm) // Funcion para rotar registro a la derecha.
+{
+	uint32_t Raux1, Raux2;   // Defino variables auxiliares
+	R1=*Rnd/pow(2,Rm);       // Muevo a la derecha Rm posiciones
+	R2=*Rnd*pow(2,(32-Rm));  // Muevo a la izquierda 32-Rm posiciones
+	*Rnd=R1+R2;              
+}
+
+void ASRS(uint32_t *Rdn, uint32_t Rm);
+{
+	
 }
