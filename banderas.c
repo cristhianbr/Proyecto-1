272c9e6e9ed3ec4596f8c4ac3f127127a0369b10
JuanDhz <juandaaaa@gmail.com>
2015-08-30 14:56:07 -0500

    Se modifico el nombre de la variable que direcciona al arreglo de las banderas por uno mas apropiado para la comprension de su labor, esta modificacion se realizo en banderas.h y banderas.c

--- banderas.c
+++ banderas.c
@@ -1,41 +1,41 @@
 #include "banderas.h"
 #include <math.h>
 
-void banderas(uint32_t Rp, uint32_t Rm, uint32_t Rn, char *Rl)
+void banderas(uint32_t Rp, uint32_t Rm, uint32_t Rn, char *Flags)
 {
     if ((1<<31)&Rp)     //sentencia para la bandera que indica numero negativo
     {
-        Rl[0]=1;
+        Flags[0]=1;
     }
     else
     {
-        Rl[0]=0;
+        Flags[0]=0;
     }
     if (Rp==0)          //sentencia para la bandera que indica el numero cero
     {
-        Rl[1]=1;
+        Flags[1]=1;
     }
     else
     {
-        Rl[1]=0;
+        Flags[1]=0;
     }
     if (((((1<<31)&Rm)&&((1<<31)&Rn))&&((0<<31)&Rp))||((((1<<31)&Rm)||((1<<31)&Rn))&&((0<<31)&Rp)))
     {                    //sentencia para la bandera que indica un carry
-        Rl[2]=1;
+        Flags[2]=1;
     }
     else
     {
-        Rl[2]=0;
+        Flags[2]=0;
     }
     if  ((((1<<31)-1)&Rm)==(((1<<31)-1)&Rn))        //sentencia para la bandera que indica que hay un sobreflujo
     {
         if((((1<<31)-1)&Rm)!=((((1<<31)-1)&Rp)))
         {
-            Rl[3]=1;
+            Flags[3]=1;
         }
     }
     else
     {
-        Rl[3]=0;
+        Flags[3]=0;
     }
 }
