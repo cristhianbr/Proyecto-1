667ec719f3f4f0ef5608c0131f1166faa86a8b32
lucho0718 <lucho.leel@gmail.com>
2015-09-08 10:29:54 -0500

    Se modifico la libreria de banderas para trabajar con macros y de esta manera asegurar que se van a modificar siempre los valores de banderas

--- banderas.c
+++ banderas.c
@@ -5,37 +5,37 @@ void banderas(uint32_t Rp, uint32_t Rm, uint32_t Rn, char *Flags)
 {
     if ((1<<31)&Rp)     //sentencia para la bandera que indica numero negativo
     {
-        Flags[0]=1;
+        Flags[N]=1;
     }
     else
     {
-        Flags[0]=0;
+        Flags[N]=0;
     }
     if (Rp==0)          //sentencia para la bandera que indica el numero cero
     {
-        Flags[1]=1;
+        Flags[Z]=1;
     }
     else
     {
-        Flags[1]=0;
+        Flags[Z]=0;
     }
     if (((((1<<31)&Rm)&&((1<<31)&Rn))&&((0<<31)&Rp))||((((1<<31)&Rm)||((1<<31)&Rn))&&((0<<31)&Rp)))
     {                    //sentencia para la bandera que indica un carry
-        Flags[2]=1;
+        Flags[C]=1;
     }
     else
     {
-        Flags[2]=0;
+        Flags[C]=0;
     }
     if  (((pow(2,31)-1)==Rm)==((pow(2,31)-1)==Rn))        //sentencia para la bandera que indica que hay un sobreflujo
     {
         if (((pow(2,31)-1)==Rm)!=((pow(2,31)-1)==Rp))
         {
-            Flags[3]=1;
+            Flags[V]=1;
         }
     }
     else
     {
-        Flags[3]=0;
+        Flags[V]=0;
     }
 }
