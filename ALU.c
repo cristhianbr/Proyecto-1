907f6b67e2b80a194919cbd8e6857bea52ae47a8
JuanDhz <juandaaaa@gmail.com>
2015-09-17 12:48:38 -0500

    complemento de la actualizacion anterior ( no significativo).

--- ALU.c
+++ ALU.c
@@ -1,31 +1,31 @@
 #include "ALU.h"
 #include "banderas.h"
 
-void add(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma aritmetica
+void ADD(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma aritmetica
 {
 	*Rp=Rm+Rn;
     banderas(*Rp,Rm,Rn,flag);
 }
-void And(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la multiplicacion logica
+void AND(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la multiplicacion logica
 {
 	*Rp=Rm&Rn;
 	banderas(*Rp,Rm,Rn,flag);
 }
-void eor(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma logica exclusiva
+void EOR(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma logica exclusiva
 {
 	*Rp=Rm^Rm;
 	banderas(*Rp,Rm,Rn,flag);
 }
-void mov(uint32_t *Rp, uint32_t Rm)			            	//funcion para mover un fichero a otra direccion
+void MOV(uint32_t *Rp, uint32_t Rm)			            	//funcion para mover un fichero a otra direccion
 {
 	*Rp=Rm;
 }
-void orr(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma logica
+void ORR(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la suma logica
 {
 	*Rp=Rm|Rn;
 	banderas(*Rp,Rm,Rn,flag);
 }
-void sub(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la resta aritmetica
+void SUB(uint32_t *Rp, uint32_t Rm, uint32_t Rn, char *flag)	//funcion de la resta aritmetica
 {
 	*Rp=Rm-Rn;
 	banderas(*Rp,Rm,Rn,flag);
