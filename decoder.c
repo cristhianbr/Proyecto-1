953350bd72392f803bec13f30f0350629dbeca19
JuanDhz <juandaaaa@gmail.com>
2015-09-19 00:50:07 -0500

    Se agrego la autodocumentacion a la libreria saltos.h; Se cambio el nombre de las variables en Saltos.h y Saltos.c por unos mas adecuados para la comprension del codigo; Se añaden los comentarios generales al codigo en decoder.c; Se actualizo el archivo Doxyfile.

--- decoder.c
+++ decoder.c
@@ -5,29 +5,35 @@ void decodeInstruction(instruction_t instruction, uint32_t *registros, char *fla
     // instruction.op1_value --> Valor primer operando
     // instruction.op1_type  --> Tipo primer operando (R->Registro #->Numero N->Ninguno)
     // ... Igual para los otros operandos
-	if(strcmp(instruction.mnemonic,"ADD")==0)
+	if(strcmp(instruction.mnemonic,"ADD")==0) //Si la intruccion es ADD :
     {
-        if((instruction.op3_type=='N')&&(instruction.op2_type=='R'))
+        if((instruction.op3_type=='N')&&(instruction.op2_type=='R')) // Si en el operando 3 no tengo nada y el operando 2 es un reguistro
         {
             ADD(&registros[instruction.op1_value],registros[instruction.op1_value],registros[instruction.op2_value],flag);
             registros[15]+=1;
         }
-        if((instruction.op3_type=='N')&&(instruction.op2_type=='#'))
+        if((instruction.op3_type=='N')&&(instruction.op2_type=='#')) // Si en el operando 3 no hay nada y el operando 2 es un inmediato
         {
             ADD(&registros[instruction.op1_value],registros[instruction.op1_value],instruction.op2_value,flag);
             registros[15]+=1;
         }
-        if(instruction.op3_type=='R')
+        if(instruction.op3_type=='R') // Si el operando 3 es un registro
         {
             ADD(&registros[instruction.op1_value],registros[instruction.op2_value],registros[instruction.op3_value],flag);
             registros[15]+=1;
         }
-        if(instruction.op3_type=='#')
+        if(instruction.op3_type=='#')//Si el operando 3 es un inmediato
         {
             ADD(&registros[instruction.op1_value],registros[instruction.op2_value],instruction.op3_value,flag);
             registros[15]+=1;
         }
     }
+        /*
+        **********************************************************************************************
+        *  Para la comparacion de los demas mnemonicos, se evaluan las mismas condiciones que en ADD *
+        **********************************************************************************************
+        */
+
     if(strcmp(instruction.mnemonic,"AND")==0)
     {
         if((instruction.op3_type=='N')&&(instruction.op2_type=='R'))
@@ -356,82 +362,109 @@ void decodeInstruction(instruction_t instruction, uint32_t *registros, char *fla
             registros[15]+=1;
         }
     }
+
+      /* SALTOS DE LINEA */
+
+      /* Se efectua la operacion correspondiente a la condicon del mnemonico */
+
     if(strcmp(instruction.mnemonic,"B")==0)
     {
         B(&registros[15],instruction.op1_value);
     }
-    if(strcmp(instruction.mnemonic,"BEQ")==0)
+
+    if(strcmp(instruction.mnemonic,"BEQ")==0) /* Salta si la bandera de Zero esta activa */
     {
         BEQ(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BNE")==0)
+
+    if(strcmp(instruction.mnemonic,"BNE")==0) /* Salta si la bandera de Zero no esta activa */
     {
         BNE(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BCS")==0)
+
+    if(strcmp(instruction.mnemonic,"BCS")==0) /* Salta si la bandera de Carry esta activa */
     {
         BCS(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BCC")==0)
+
+    if(strcmp(instruction.mnemonic,"BCC")==0) /* Salta si la bandera de Carry no esta activa */
     {
         BCC(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BMI")==0)
+
+    if(strcmp(instruction.mnemonic,"BMI")==0) /* Salta si la bandera de Negativo esta activa */
     {
         BMI(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BPL")==0)
+
+    if(strcmp(instruction.mnemonic,"BPL")==0) /* Salta si la bandera de Negativo no esta activa */
     {
         BPL(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BVS")==0)
+
+    if(strcmp(instruction.mnemonic,"BVS")==0) /* Salta si la bandera de Sobreflujo esta activa */
     {
         BVS(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BVC")==0)
+
+    if(strcmp(instruction.mnemonic,"BVC")==0) /* Salta si la bandera de Sobreflujo no esta activa */
     {
         BVC(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BHI")==0)
+
+    if(strcmp(instruction.mnemonic,"BHI")==0) /* Salta si la bandera de Carry esta activa y la bandera de Zero no esta acativa */
     {
         BHI(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BLS")==0)
+
+    if(strcmp(instruction.mnemonic,"BLS")==0) /* Salta si la bandera de Carry no esta activa y la bandera de Zero esta acativa */
     {
         BLS(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BGE")==0)
+
+    if(strcmp(instruction.mnemonic,"BGE")==0) /* Salta si la bandera de Sobreflujo y la bandera de Negativo estan ambas activas o ambas inactivas */
     {
         BGE(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BLT")==0)
+
+    if(strcmp(instruction.mnemonic,"BLT")==0) /* Salta si la bandera de Sobleflijo esta activa y la de NEgativo esta inactiva, o viceversa*/
     {
         BLT(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BGT")==0)
+
+    if(strcmp(instruction.mnemonic,"BGT")==0) /* Salta si la bandera de Carry esta activa y la bandera de Zero no esta acativa */
     {
         BGT(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BLE")==0)
+
+    if(strcmp(instruction.mnemonic,"BLE")==0) /* Salta si la bandera de Zero esta inactiva y la bandera de Negatvo esta en el mismo estado de la de Sobreflujo*/
     {
         BLE(&registros[15],instruction.op1_value,flag);
     }
-    if(strcmp(instruction.mnemonic,"BLA")==0)
+
+    if(strcmp(instruction.mnemonic,"BLA")==0) /* Salta un inmediato en PC */
     {
         BLA(&registros[15],instruction.op1_value);
     }
-    if(strcmp(instruction.mnemonic,"BL")==0)
+
+    if(strcmp(instruction.mnemonic,"BL")==0) /*Esta funcion envia al PC a una direccion inmediata y manda al LR a la siguiente instruccion teniendo en cuenta
+                                             la consideracion de que PC emplea 32 bits*/
     {
         BL(&registros[15],&registros[14],instruction.op1_value);
     }
-    if(strcmp(instruction.mnemonic,"BX")==0)
+
+    if(strcmp(instruction.mnemonic,"BX")==0) /*Esta funcion guardo el contenido de un registro en el PC y pone a LR en la siguiente instruccion teniendo en
+                                             cuenta la consideracion de que PC emplea 32 bits*/
     {
         BX(&registros[15],registros[instruction.op1_value]);
     }
-    if(strcmp(instruction.mnemonic,"BLX")==0)
+
+    if(strcmp(instruction.mnemonic,"BLX")==0) /*Esta funcion guardo el contenido de un registro en el PC y pone a LR en la siguiente instruccion teniendo
+                                                en cuenta la consideracion de que PC emplea 32 bits*/
     {
         BLX(&registros[15],&registros[14],registros[instruction.op1_value]);
     }
+
 }
 
 instruction_t getInstruction(char* instStr)
--- saltos.h
+++ saltos.h
@@ -1,37 +1,145 @@
-void B(uint32_t *reg, int salto);
+/**
+*\brief Funcion que suma a la posicion actual un inmediato, y llega a la pasicion en el PC deseada.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*/
+void B(uint32_t *reg, int inm);
 
-void BEQ(uint32_t *reg, int salto, char *flags);
+/**
+*\brief  Funcion que salta si la bandera de Zero esta activa .
+*\param *reg Direccion del PC.
+*\param inm  Inmediato.
+*/
+void BEQ(uint32_t *reg, int inm, char *flags);
 
-void BNE(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Zero no esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BNE(uint32_t *reg, int inm, char *flags);
 
-void BCS(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Carry esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BCS(uint32_t *reg, int inm, char *flags);
 
-void BCC(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Carry no esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BCC(uint32_t *reg, int inm, char *flags);
 
-void BMI(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Negativo esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BMI(uint32_t *reg, int inm, char *flags);
 
-void BPL(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Negativo no esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BPL(uint32_t *reg, int inm, char *flags);
 
-void BVS(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que alta si la bandera de Sobreflujo esta activa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BVS(uint32_t *reg, int inm, char *flags);
 
-void BVC(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que Salta si la bandera de Sobreflujo no esta activa
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BVC(uint32_t *reg, int inm, char *flags);
 
-void BHI(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Carry esta activa y la bandera de Zero no esta acativa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BHI(uint32_t *reg, int inm, char *flags);
 
-void BLS(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Carry no esta activa y la bandera de Zero esta acativa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BLS(uint32_t *reg, int inm, char *flags);
 
-void BGE(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Sobreflujo y la bandera de Negativo estan ambas activas o ambas inactivas.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BGE(uint32_t *reg, int inm, char *flags);
 
-void BLT(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion salta si la bandera de Sobleflijo esta activa y la de NEgativo esta inactiva, o viceversa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BLT(uint32_t *reg, int inm, char *flags);
 
-void BGT(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que alta si la bandera de Carry esta activa y la bandera de Zero no esta acativa.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BGT(uint32_t *reg, int inm, char *flags);
 
-void BLE(uint32_t *reg, int salto, char *flags);
+/**
+*\brief Funcion que salta si la bandera de Zero esta inactiva y la bandera de Negatvo esta en el mismo estado de la de Sobreflujo.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*\param *flags Direccion del arreglo de banderas
+*/
+void BLE(uint32_t *reg, int inm, char *flags);
 
-void BLA(uint32_t *reg, int salto);
+/**
+*\brief Funcion que salta un inmediato en PC .
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*/
+void BLA(uint32_t *reg, int inm);
 
-void BL(uint32_t *reg, uint32_t *reg2, int salto);
+/**
+*\brief Funcion queenvia al PC a una direccion inmediata y manda al LR a la siguiente instruccion teniendo en cuenta la consideracion de que PC emplea 32 bits.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*/
+void BL(uint32_t *reg, uint32_t *reg2, int inm);
 
+/**
+*\brief Funcion que guarda el contenido de un registro en el PC y pone a LR en la siguiente instruccion teniendo en cuenta la consideracion de que PC emplea 32 bits.
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*/
 void BX(uint32_t *reg, uint32_t Rm);
 
+/**
+*\brief Esta funcion guardo el contenido de un registro en el PC y pone a LR en la siguiente instruccion teniendo en cuenta la consideracion de que PC emplea 32 bits
+*\param *reg Direccion del PC.
+*\param inm Inmediato.
+*/
 void BLX(uint32_t *reg, uint32_t *reg2, uint32_t Rm);
