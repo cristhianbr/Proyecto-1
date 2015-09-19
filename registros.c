9bcd6800319f8a5f345a91d2c5039818899409da
lucho0718 <lucho.leel@gmail.com>
2015-09-18 20:51:05 -0500

    Se creo una libreria llamada saltos con sus respectivos .c y .h, se agregaron en esta todas las funciones de saltos y ademas se extendieron los elementos de reg con el fin de poder trabajar con el reg[15] PC y reg[14 LR]

--- registros.c
+++ registros.c
@@ -6,7 +6,7 @@ void mostrar_registro(uint32_t *reg)
 {
 	int i;
 	move(4, 15); printw("Registros");
-	for(i=0;i<=12;i++)          //se crea un ciclo for para dar valores a los registros y mostrar los registros en pantalla
+	for(i=0;i<=15;i++)          //se crea un ciclo for para dar valores a los registros y mostrar los registros en pantalla
 	{
 		move(6+i, 15);
 		printw("R%d:\t%.10d\n", i,reg[i]);
---
+++ saltos.h
@@ -0,0 +1,37 @@
+void B(uint32_t *reg, int salto);
+
+void BEQ(uint32_t *reg, int salto, char *flags);
+
+void BNE(uint32_t *reg, int salto, char *flags);
+
+void BCS(uint32_t *reg, int salto, char *flags);
+
+void BCC(uint32_t *reg, int salto, char *flags);
+
+void BMI(uint32_t *reg, int salto, char *flags);
+
+void BPL(uint32_t *reg, int salto, char *flags);
+
+void BVS(uint32_t *reg, int salto, char *flags);
+
+void BVC(uint32_t *reg, int salto, char *flags);
+
+void BHI(uint32_t *reg, int salto, char *flags);
+
+void BLS(uint32_t *reg, int salto, char *flags);
+
+void BGE(uint32_t *reg, int salto, char *flags);
+
+void BLT(uint32_t *reg, int salto, char *flags);
+
+void BGT(uint32_t *reg, int salto, char *flags);
+
+void BLE(uint32_t *reg, int salto, char *flags);
+
+void BLA(uint32_t *reg, int salto);
+
+void BL(uint32_t *reg, int salto);
+
+void BX(uint32_t *reg, uint32_t Rm);
+
+void BLX(uint32_t *reg, uint32_t Rm);
